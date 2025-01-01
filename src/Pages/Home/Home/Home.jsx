ecc8325d79dc94c44790063d52d7b402fc5b2b8e
alimtorofder1 <alimtorofder43@gmail.com>
2025-01-01 21:06:06 +0600

    first commit

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -11,20 +11,19 @@ import ServiceList from '../ServiceList/ServiceList'
 const Home = () => {
     return (
         <div>
-
-      
-        <div className=' grid grid-cols-1 md:grid-cols-2  gap-4 place-content-between h-48 '>
+        <div className='flex grid grid-cols-1 md:grid-cols-2  gap-4 place-content-between h-48 '>
           <div className="pl-5 mt-36 ">
           <h2 className="text-4xl font-bold ">BEAUTY SALON <br />
           FOR EVERY MEN</h2>
           <p className="text-gray-600 mt-8">Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Purus commodo ipsum duis <br />laoreet maecenas. Feugiat </p>
           <div className='mt-8'>
           <button className="button w-48 h-10 text-white">Get an Appointment</button>
-          </div>
-          </div>
-         <div className='md:ml-32 w-full'>
+          <div className='md:ml-64  w-full'>
          <img src={banner} alt="" srcset="" />
          </div>
+          </div>
+          </div>
+     
          </div>
          <AboutUs></AboutUs>
          <ServiceList></ServiceList>
